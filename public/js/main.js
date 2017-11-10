dc24cda5e49eb7a5a8422d116558c3d5dccf5c8c
Eric Chung <rchung.eric@gmail.com>
2017-11-10 14:33:57 +1100

    made changes to navbar ui

--- public/js/main.js
+++ public/js/main.js
@@ -5,14 +5,38 @@ $(document).ready(function(){
     var typed = new Typed(".heading", options);
 });
 
-
-
 $(window).on('load', function () {
     AOS.refresh();
 });
 
+$(window).scroll(function() {
+    let vph = $(window).height();
+
+    if ($(this).scrollTop() > 80 && $(this).scrollTop() < vph) {
+        $('.navigation').fadeOut(200);
+    } else if ($(this).scrollTop()>vph) {
+        lowerNavCSS();
+        $('.navigation').fadeIn(200);
+    } else {
+        topNavCSS();
+        $('.navigation').fadeIn(200);
+    }
+});
+
+function topNavCSS() {
+    $('.navigation').css("background", "");
+    $(".navigation").find("a").css("color", "#252525");
+    $('.navigation').find("li.resume-link").css("border", "2px solid #252525");
+}
+
+function lowerNavCSS() {
+    $('.navigation').css("background", "black");
+    $(".navigation").find("a").css("color", "#DADADA");
+    $('.navigation').find("li.resume-link").css("border", "2px solid #DADADA");
+}
+
 function resizeDiv() {
     let vph = $(window).height();
-    let navh = $(".navigation").height();
+    //let navh = $(".navigation").height();
     $(".banner").css({"height": vph + "px"});
 }
